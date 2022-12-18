337825895bb23a67dbe3141461ba5f0200de3455
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2022-12-18 17:46:23 +0700

    Update DB for menu access

--- db_app_loket.sql
+++ db_app_loket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2022 at 04:26 PM
+-- Generation Time: Dec 18, 2022 at 11:45 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -41,8 +41,8 @@ CREATE TABLE `tb_counter` (
 --
 
 INSERT INTO `tb_counter` (`counter_id`, `counter_sequence`, `counter_name`, `date_created`, `created_by`, `status`) VALUES
-('lkh6868AC1244', 1, 'Loket 1', '2022-12-10 23:32:42', 'Administrator', 'open'),
-('lkh6868AC1245', 2, 'Loket 2', '2022-12-10 23:32:42', 'Administrator', 'close');
+('lkh6868AC1244', 1, 'Loket 1', '2022-12-10 23:32:42', 'Administrator', 'close'),
+('lkh6868AC1245', 2, 'Loket 2', '2022-12-10 23:32:42', 'Ridwan Naim', 'open');
 
 -- --------------------------------------------------------
 
@@ -70,7 +70,7 @@ INSERT INTO `tb_menu` (`menu_id`, `menu_sequence`, `grup_fitur`, `label_menu`, `
 (2, 0, 'user', 'Tambah User', 'tambah-user', 'fas fa-plus-square', 'button', 'w'),
 (3, 0, 'user', 'Ubah User', 'ubah-user', 'fas fa user-edit', 'button', 'w'),
 (4, 0, 'user', 'Hapus User', 'hapus-user', 'fas fa-trash-alt', 'button', 'w'),
-(5, 3, 'akses', 'Grup User', 'grup-user', 'fas fa-key', 'mainmenu', 'r'),
+(5, 1, 'akses', 'Grup User', 'grup-user', 'fas fa-users-cog', 'submenu', 'r'),
 (6, 0, 'akses', 'Tambah Grup', 'tambah-grup', 'fas fa-plus-square', 'button', 'w'),
 (7, 0, 'akses', 'Ubah Grup', 'ubah-grup', 'fas fa-edit', 'button', 'w'),
 (8, 0, 'akses', 'Hapus Grup', 'hapus-grup', 'fas fa-trash-alt', 'button', 'w'),
@@ -84,18 +84,19 @@ INSERT INTO `tb_menu` (`menu_id`, `menu_sequence`, `grup_fitur`, `label_menu`, `
 (16, 0, 'konten', 'Tambah Teks', 'tambah-teks', 'fas fa-plus-square', 'button', 'w'),
 (17, 0, 'konten', 'Hapus Teks', 'hapus-teks', 'fas fa-trash-alt', 'button', 'w'),
 (18, 4, 'loket', 'Loket', '#loket', 'fas fa-concierge-bell', 'mainmenu', 'r'),
-(19, 1, 'loket', 'Operator', 'operator', 'fas fa-user-tag', 'submenu', 'r'),
-(20, 0, 'loket', 'Tambah Operator', 'tambah-operator', 'fas fa-user-plus', 'button', 'w'),
-(21, 0, 'loket', 'Hapus Operator', 'hapus-operator', 'fas fa-user-times', 'button', 'w'),
-(22, 0, 'loket', 'Ubah Operator', 'ubah-operator', 'fas fa-user-edit', 'button', 'w'),
-(23, 2, 'loket', 'Loket', 'loket', 'fas fa-chalkboard-teacher', 'submenu', 'r'),
+(19, 2, 'akses', 'Operator', 'operator', 'fas fa-user-tag', 'submenu', 'r'),
+(20, 0, 'akses', 'Tambah Operator', 'tambah-operator', 'fas fa-user-plus', 'button', 'w'),
+(21, 0, 'akses', 'Hapus Operator', 'hapus-operator', 'fas fa-user-times', 'button', 'w'),
+(22, 0, 'akses', 'Ubah Operator', 'ubah-operator', 'fas fa-user-edit', 'button', 'w'),
+(23, 1, 'loket', 'Loket', 'loket', 'fas fa-chalkboard-teacher', 'submenu', 'r'),
 (24, 0, 'loket', 'Tambah Loket', 'tambah-loket', 'fas fa-plus-square', 'button', 'w'),
 (25, 0, 'loket', 'Ubah Loket', 'ubah-loket', 'fas fa-edit', 'button', 'w'),
 (26, 0, 'loket', 'Hapus Loket', 'hapus-loket', 'fas fa-trash-alt', 'button', 'w'),
-(27, 3, 'loket', 'Transaksi', 'transaksi', 'fas fa-clipboard-list', 'submenu', 'r'),
+(27, 2, 'loket', 'Transaksi', 'transaksi', 'fas fa-clipboard-list', 'submenu', 'r'),
 (28, 0, 'konten', 'Ubah Gambar', 'ubah-gambar', 'fas fa-edit', 'button', 'w'),
 (29, 0, 'konten', 'Ubah Teks', 'ubah-teks', 'fas fa-edit', 'button', 'w'),
-(30, 0, 'loket', 'Masuk Loket', 'masuk-loket', 'fas fa-signin', 'button', 'w');
+(30, 0, 'loket', 'Masuk Loket', 'masuk-loket', 'fas fa-signin', 'button', 'w'),
+(31, 3, 'akses', 'Manajemen Akses', '#manajemen-akses', 'fas fa-key', 'mainmenu', 'r');
 
 -- --------------------------------------------------------
 
@@ -115,8 +116,7 @@ CREATE TABLE `tb_operator` (
 
 INSERT INTO `tb_operator` (`operator_id`, `counter_id`, `user_id`) VALUES
 ('0a256188b3d05', 'lkh6868AC1244', '1aD98123944Ac'),
-('2432310c74651', NULL, '1aD98123944Ac'),
-('2b1ae23f44c8c', 'lkh6868AC1245', '1aD98123944Ac');
+('2b1ae23f44c8c', 'lkh6868AC1245', '731e2c7521312');
 
 -- --------------------------------------------------------
 
@@ -135,36 +135,54 @@ CREATE TABLE `tb_roles` (
 --
 
 INSERT INTO `tb_roles` (`role_id`, `group_id`, `menu_id`) VALUES
-(142, 1, 1),
-(143, 1, 2),
-(144, 1, 3),
-(145, 1, 4),
-(146, 1, 5),
-(147, 1, 6),
-(148, 1, 7),
-(149, 1, 8),
-(150, 1, 18),
-(151, 1, 19),
-(152, 1, 23),
-(153, 1, 27),
-(154, 1, 20),
-(155, 1, 21),
-(156, 1, 22),
-(157, 1, 24),
-(158, 1, 25),
-(159, 1, 26),
-(160, 1, 30),
-(161, 1, 9),
-(162, 1, 11),
-(163, 1, 12),
-(164, 1, 15),
-(165, 1, 13),
-(166, 1, 14),
-(167, 1, 16),
-(168, 1, 17),
-(169, 1, 28),
-(170, 1, 29),
-(171, 1, 10);
+(193, 1, 1),
+(194, 1, 2),
+(195, 1, 3),
+(196, 1, 4),
+(197, 1, 5),
+(198, 1, 19),
+(199, 1, 31),
+(200, 1, 6),
+(201, 1, 7),
+(202, 1, 8),
+(203, 1, 20),
+(204, 1, 21),
+(205, 1, 22),
+(206, 1, 18),
+(207, 1, 23),
+(208, 1, 27),
+(209, 1, 24),
+(210, 1, 25),
+(211, 1, 26),
+(212, 1, 30),
+(213, 1, 9),
+(214, 1, 11),
+(215, 1, 12),
+(216, 1, 15),
+(217, 1, 13),
+(218, 1, 14),
+(219, 1, 16),
+(220, 1, 17),
+(221, 1, 28),
+(222, 1, 29),
+(223, 1, 10),
+(224, 5, 18),
+(225, 5, 23),
+(226, 5, 27),
+(227, 5, 24),
+(228, 5, 25),
+(229, 5, 26),
+(230, 5, 30),
+(231, 5, 9),
+(232, 5, 11),
+(233, 5, 12),
+(234, 5, 15),
+(235, 5, 13),
+(236, 5, 14),
+(237, 5, 16),
+(238, 5, 17),
+(239, 5, 28),
+(240, 5, 29);
 
 -- --------------------------------------------------------
 
@@ -234,11 +252,20 @@ CREATE TABLE `tb_transaction` (
 --
 
 INSERT INTO `tb_transaction` (`transaction_id`, `queue_num`, `counter_id`, `employee_name`, `date`, `status`) VALUES
+('22fe25b431b56', 4, 'lkh6868AC1245', NULL, '2022-12-18 11:41:15', 'queue'),
+('23b64e345c923', 4, 'lkh6868AC1245', NULL, '2022-12-15 04:27:59', 'queue'),
+('284266d4c413a', 3, 'lkh6868AC1245', 'Ridwan Naim', '2022-12-18 10:24:50', 'done'),
+('5f1aa3e7fc48b', 2, 'lkh6868AC1244', NULL, '2022-12-18 11:39:42', 'queue'),
 ('6b0ecb03eabb3', 3, 'lkh6868AC1244', 'Administrator', '2022-12-15 04:06:35', 'done'),
 ('6dbd74fa028a5', 6, 'lkh6868AC1244', NULL, '2022-12-15 04:26:14', 'queue'),
 ('84b7dc791335c', 5, 'lkh6868AC1244', 'Administrator', '2022-12-15 04:25:37', 'done'),
+('a38e9b7b9f16a', 3, 'lkh6868AC1245', NULL, '2022-12-15 04:27:49', 'queue'),
 ('a79ef926f976d', 2, 'lkh6868AC1244', 'Administrator', '2022-12-15 03:51:17', 'done'),
+('a95ca52066faa', 1, 'lkh6868AC1244', NULL, '2022-12-18 11:38:44', 'queue'),
+('d31506c8b719c', 2, 'lkh6868AC1245', 'Ridwan Naim', '2022-12-18 09:12:50', 'done'),
+('dd9e02e53929c', 1, 'lkh6868AC1245', 'Ridwan Naim', '2022-12-18 09:02:07', 'done'),
 ('df5aeb5797078', 1, 'lkh6868AC1245', 'Administrator', '2022-12-15 03:51:33', 'done'),
+('e0f306f949e3e', 3, 'lkh6868AC1244', NULL, '2022-12-18 11:42:34', 'queue'),
 ('e740c50fceda5', 4, 'lkh6868AC1244', 'Administrator', '2022-12-15 04:09:45', 'done'),
 ('f7aa8aa6f0c77', 2, 'lkh6868AC1245', 'Administrator', '2022-12-15 04:11:48', 'done'),
 ('fb41c0581a682', 1, 'lkh6868AC1244', 'Administrator', '2022-12-15 00:00:00', 'done');
@@ -267,7 +294,8 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`user_id`, `group_id`, `employee_name`, `username`, `password`, `date_created`, `last_login`, `last_ip`, `user_picture`, `status`) VALUES
-('1aD98123944Ac', 1, 'Administrator', 'admin', '$argon2id$v=19$m=2048,t=8,p=4$eEw5b2ZST1FwNUkxZERjbw$/AjMoKqTkACT9cLjwPhkIo5rSHnYr6REjDaJ68DgQkc', '2022-12-05 16:17:14', '2022-12-13 16:48:56', '::1', 'Administrator-1aD98123944Ac-20221215121514.jpg', 'active');
+('1aD98123944Ac', 1, 'Administrator', 'admin', '$argon2id$v=19$m=2048,t=8,p=4$eEw5b2ZST1FwNUkxZERjbw$/AjMoKqTkACT9cLjwPhkIo5rSHnYr6REjDaJ68DgQkc', '2022-12-05 16:17:14', '2022-12-18 15:25:48', '::1', 'Administrator-1aD98123944Ac-20221215121514.jpg', 'active'),
+('731e2c7521312', 5, 'Ridwan Naim', 'ridwannaim', '$argon2id$v=19$m=2048,t=8,p=4$dmtnY3VuQmFvbk1lck83Vw$8i5r2mcG8f+xT71EgE95SbDFb8WTUzastMRjqLGqTp4', '2022-12-18 14:30:06', '2022-12-18 15:24:11', '::1', 'Ridwan_Naim-731e2c7521312-20221218083439.jpg', 'active');
 
 -- --------------------------------------------------------
 
@@ -287,7 +315,8 @@ CREATE TABLE `tb_user_group` (
 --
 
 INSERT INTO `tb_user_group` (`group_id`, `group_name`, `index_menu`, `mode`) VALUES
-(1, 'Administrator', 'dashboard', 'rw');
+(1, 'Administrator', 'dashboard', 'rw'),
+(5, 'operator', 'dashboard', 'rw');
 
 -- --------------------------------------------------------
 
@@ -391,19 +420,19 @@ ALTER TABLE `tb_web_setting`
 -- AUTO_INCREMENT for table `tb_menu`
 --
 ALTER TABLE `tb_menu`
-  MODIFY `menu_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `menu_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `tb_roles`
 --
 ALTER TABLE `tb_roles`
-  MODIFY `role_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
+  MODIFY `role_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=241;
 
 --
 -- AUTO_INCREMENT for table `tb_user_group`
 --
 ALTER TABLE `tb_user_group`
-  MODIFY `group_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `group_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_web_setting`
